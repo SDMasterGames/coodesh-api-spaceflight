bc81a484311fafd73e4d4c8b5e5949e315f29ec3
SDMasterGames <sdmastergames@hotmail.com>
2022-05-23 00:27:03 -0400

    repo fake para os testes

---
+++ __tests__/ArticleTestRepository.ts
@@ -0,0 +1,63 @@
+import { Article } from "@prisma/client";
+import { randomUUID } from "crypto";
+import {
+  IArticlesRepository,
+  ICreateArticleData,
+  IfindByIdAndUpdateData,
+} from "../src/repositories/IArticlesRepository";
+
+const ArticleRepo: Article[] = [];
+
+interface ICreateArticleDataMock extends ICreateArticleData {
+  id: string;
+}
+
+export class ArticleTestRepository implements IArticlesRepository {
+  async createArticle(data: ICreateArticleDataMock): Promise<Article> {
+    return new Promise((resolve, reject) => {
+      const articleNew = {
+        ...data,
+        featured: data.featured || false,
+        summary: data.summary || "",
+      };
+      ArticleRepo.push(articleNew);
+
+      resolve(articleNew);
+    });
+  }
+
+  async findArticleById(id: string): Promise<Article | null> {
+    return new Promise((resolve, reject) => {
+      const article = ArticleRepo.find((article) => article.id === id);
+      resolve(article || null);
+    });
+  }
+  async getAllArticles(): Promise<Article[]> {
+    return new Promise((resolve, reject) => {
+      resolve(ArticleRepo);
+    });
+  }
+
+  async deleteById(id: string): Promise<Article | null> {
+    return new Promise((resolve, reject) => {
+      const article = ArticleRepo.find((article) => article.id === id);
+      if (article) {
+        ArticleRepo.splice(ArticleRepo.indexOf(article), 1);
+      }
+      resolve(article || null);
+    });
+  }
+
+  async findByIdAndUpdate(
+    id: string,
+    data: IfindByIdAndUpdateData
+  ): Promise<Article> {
+    return new Promise((resolve, reject) => {
+      const article = ArticleRepo.find((article) => article.id === id);
+      if(article){
+        const result = { ...article, ...data };
+        resolve(result)
+      }
+    });
+  }
+}

e6fe396e7f65cdb2fd6432e7ff76e156929d6337
SDMasterGames <sdmastergames@hotmail.com>
2022-05-23 20:40:47 -0400

    implementando validação dos dados enviados

--- src/modules/articles/getAllArticles/getAllArticlesUseCase.ts
+++ src/modules/articles/getAllArticles/getAllArticlesUseCase.ts
@@ -1,10 +1,20 @@
 import { IArticlesRepository } from "../../../repositories/IArticlesRepository";
+import { IGetAllArticlesDTO } from "./getAllArticlesDTO";
 
 export class getAllArticlesUseCase {
   constructor(private IArticlesRepository: IArticlesRepository) {}
 
-  async execute() {
-    const allArticles = await this.IArticlesRepository.getAllArticles();
+  async execute({ limit, page }: IGetAllArticlesDTO) {
+    const numberLimit = Number(limit || 10);
+    const numberPage = Number(page || 1);
+    if (!Number.isInteger(numberLimit) || !Number.isInteger(numberPage)) {
+      throw new Error("Invalid limit or page");
+    }
+
+    const allArticles = await this.IArticlesRepository.getAllArticles({
+      limit: numberLimit,
+      page: numberPage,
+    });
     return allArticles || [];
   }
 }
